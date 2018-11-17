01ca64d2f3fd6db510cf61428fa1f5e956c36240
Kevin Tang <Kevin.Tang@bentley.com>
2018-11-17 10:52:04 +0800

    FX: fixed the bug for increased.js.

--- numbers/increased.js
+++ numbers/increased.js
@@ -1,6 +1,6 @@
 // Given an integer array, pick the most integers which constitute an increased sequence
 // Note that the order of integers appearing in the sequence may not be switched.
-// e.g. given an array: 1,2,5,4,3,7,8,0,9, then it will output 1,2,3,7,8,9
+// e.g. given an array: 1,2,5,4,3,7,8,0,9,6 then it will output 1,2,3,7,8,9
 
 let hash = {};
 function process(arr, i) {
@@ -24,7 +24,17 @@ function process(arr, i) {
 }
 
 function increased(arr) {
-  return process(arr, arr.length - 1);
+  for (let i = 0; i < arr.length; i++)
+    process(arr, i);
+  let max = 0, s;
+  for (let i = 0; i < arr.length; i++) {
+    let tmp = hash[i].length;
+    if (tmp > max) {
+      max = tmp;
+      s = i;
+    }
+  }
+  return hash[s];
 }
 
-console.log(increased([1,2,5,4,3,7,8,0,9]));
+console.log(increased([1,2,5,4,3,7,8,0,9,6]));
