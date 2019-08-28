b30f74d58952d524f4e1338c8900d95b1361222c
tang415a <tang415a@163.com>
2019-08-28 10:32:54 +0800

    added a few cases

---
+++ games/friendCircles.cpp
@@ -0,0 +1,85 @@
+/*
+There are N students in a class. Some of them are friends, while some are not. Their friendship is transitive in nature. For example, if A is a direct friend of B, and B is a direct friend of C, then A is an indirect friend of C. And we defined a friend circle is a group of students who are direct or indirect friends.
+
+Given a N*N matrix M representing the friend relationship between students in the class. If M[i][j] = 1, then the ith and jth students are direct friends with each other, otherwise not. And you have to output the total number of friend circles among all the students.
+
+Example 1:
+Input: 
+[[1,1,0],
+ [1,1,0],
+ [0,0,1]]
+
+Output: 2
+Explanation:The 0th and 1st students are direct friends, so they are in a friend circle. 
+The 2nd student himself is in a friend circle. So return 2.
+
+Example 2:
+Input: 
+[[1,1,0],
+ [1,1,1],
+ [0,1,1]]
+
+Output: 1
+Explanation:The 0th and 1st students are direct friends, the 1st and 2nd students are direct friends, 
+so the 0th and 2nd students are indirect friends. All of them are in the same friend circle, so return 1.
+
+Note:
+N is in range [1,200].
+M[i][i] = 1 for all students.
+If M[i][j] = 1, then M[j][i] = 1.
+*/
+#include <stdio.h>
+#include <vector>
+#include <map>
+using namespace std;
+
+void initiate(map<int, int>& m, const vector<vector<int>>& matrix) {
+    unsigned int N = matrix.size();
+    for (unsigned int i = 1; i < N; i++) {
+        for (unsigned int j = 0; j < i; j++) {
+            if (matrix[i][j] > 0)
+                m[i] = j;
+        }
+    }
+}
+
+bool isFriend(int i, int j, const map<int, int>& m) {
+    while (m.find(i) != m.end()) {
+        i = m.at(i);
+    }
+    while (m.find(j) != m.end()) {
+        j = m.at(j);
+    }
+    return i == j;
+}
+
+bool isNewCircle(int i, const vector<int>& vec, const map<int, int>& m) {
+    for (auto& v : vec) {
+        if (isFriend(i, v, m))
+            return false;
+    }
+    return true;
+}
+
+int findCircleNum(vector<vector<int>>& matrix) {
+    map<int, int> m;
+    initiate(m, matrix);
+    unsigned int N = matrix.size();
+    vector<int> vec;
+    for (unsigned int i = 0; i < N; i++) {
+        if (isNewCircle(i, vec, m))
+            vec.push_back(i);
+    }
+    return vec.size();
+}
+
+int main() {
+    vector<vector<int>> v = { { 1,1,0 },{ 1,1,0 },{ 0,0,1 } };
+    int num = findCircleNum(v);
+    printf("%d\n", num);
+    v = { { 1,1,0 },{ 1,1,1 },{ 0,1,1 } };
+    num = findCircleNum(v);
+    printf("%d\n", num);
+    getchar();
+    return 0;
+}
---
+++ strings/detectCapital.cpp
@@ -0,0 +1,56 @@
+/*
+Given a word, you need to judge whether the usage of capitals in it is right or not.
+
+We define the usage of capitals in a word to be right when one of the following cases holds:
+
+All letters in this word are capitals, like "USA".
+All letters in this word are not capitals, like "leetcode".
+Only the first letter in this word is capital, like "Google".
+Otherwise, we define that this word doesn't use capitals in a right way.
+ 
+
+Example 1:
+
+Input: "USA"
+Output: True
+ 
+
+Example 2:
+
+Input: "FlaG"
+Output: False
+ 
+
+Note: The input will be a non-empty word consisting of uppercase and lowercase latin letters.
+*/
+
+#include <stdio.h>
+#include <string>
+using namespace std;
+
+bool detectCapitalUse(const string& word) {
+  bool continued = true;
+  unsigned int len = word.length();
+  for (unsigned int i = 0; i < len; i++) {
+    if (word[i] >= 'a' && word[i] <= 'z') { // lowercase  
+      if (i > 1 && continued)
+        return false;
+      continued = false;
+    }    
+    else if (!continued)
+      return false;
+  }
+  return true;
+}
+
+int main() {
+  string w[] = {"USA", "FlaG", "TItanic"};
+  for (int i = 0; i < sizeof(w)/sizeof(w[0]); i++) {
+    if (detectCapitalUse(w[i]))
+      printf("%d true\n", i);
+    else
+      printf("%d false\n", i);
+  }
+  getchar();
+  return 0;
+}
