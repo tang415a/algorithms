432b796dc1a087d949ad7dd65d1bc95588b9339e
tang415a <tang415a@163.com>
2019-10-13 17:22:00 +0800

    added

---
+++ strings/minAddValidParentheses.cpp
@@ -0,0 +1,77 @@
+/*
+
+Given a string S of '(' and ')' parentheses, we add the minimum number of parentheses ( '(' or ')', and in any positions ) so that the resulting parentheses string is valid.
+
+Formally, a parentheses string is valid if and only if:
+
+It is the empty string, or
+It can be written as AB (A concatenated with B), where A and B are valid strings, or
+It can be written as (A), where A is a valid string.
+Given a parentheses string, return the minimum number of parentheses we must add to make the resulting string valid.
+
+Example 1:
+
+Input: "())"
+Output: 1
+
+Example 2:
+
+Input: "((("
+Output: 3
+
+Example 3:
+
+Input: "()"
+Output: 0
+
+Example 4:
+
+Input: "()))(("
+Output: 4
+
+Note:
+
+S.length <= 1000
+S only consists of '(' and ')' characters.
+*/
+
+#include <stdio.h>
+#include <string>
+using namespace std;
+
+int minAddValidateParentheses(string& s) {
+  int res = 0, sum = 0;
+  for(auto &c: s) {
+    if (c == '(') {
+      if (sum < 0) {
+        res -= sum;
+        sum = 0;
+      }
+      sum++;
+    }
+    else if (c == ')') {
+      sum--;
+    }
+  }
+  res += (sum < 0 ? -sum : sum);
+  return res;
+}
+
+int minAddValidateParentheses2(string& s) {
+  int l = 0, m = 0;
+  for(auto& c: s) {
+    if(c == '(') ++l;
+    else if(c == ')' && l > 0) {
+      ++m;
+      --l;
+    }
+  }
+  return s.size() - m * 2;
+}
+
+int main() {
+  string s = "()))((";
+  printf("%d", minAddValidateParentheses2(s));
+  getchar();
+  return 0;
+}
