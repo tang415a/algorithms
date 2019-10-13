49e4e700071c4255fa6a695ab544dbd5e82eb67f
tang415a <tang415a@163.com>
2019-10-13 20:20:32 +0800

    new case

---
+++ numbers/threeSumWithMulti.cpp
@@ -0,0 +1,76 @@
+/*
+Given an integer array A, and an integer target, return the number of tuples i, j, k  such that i < j < k and A[i] + A[j] + A[k] == target.
+
+As the answer can be very large, return it modulo 10^9 + 7.
+
+Example 1:
+
+Input: A = [1,1,2,2,3,3,4,4,5,5], target = 8
+Output: 20
+Explanation: 
+Enumerating by the values (A[i], A[j], A[k]):
+(1, 2, 5) occurs 8 times;
+(1, 3, 4) occurs 8 times;
+(2, 2, 4) occurs 2 times;
+(2, 3, 3) occurs 2 times.
+
+Example 2:
+
+Input: A = [1,1,2,2,2,2], target = 5
+Output: 12
+Explanation: 
+A[i] = 1, A[j] = A[k] = 2 occurs 12 times:
+We choose one 1 from [1,1] in 2 ways,
+and two 2s from [2,2,2,2] in 6 ways.
+
+Note:
+
+3 <= A.length <= 3000
+0 <= A[i] <= 100
+0 <= target <= 300
+*/
+
+#include <stdio.h>
+#include <vector>
+using namespace std;
+
+int threeSumMulti(vector<int>& A, int target) {
+  const int kMaxN = 100;
+  const int kMod = 10e9 + 7;
+  vector<int> vec(kMaxN + 1, 0);
+  for(auto& v: A) {
+    vec[v]++;
+  }
+  long long res = 0;
+  for(int i = 1; i <= target / 3; i++) {
+    if (vec[i] == 0)
+      continue;
+    for(int j = i; j <= target / 2; j++) {
+      if (vec[j] == 0)
+        continue;
+      int k = target - j - i;
+      if (k < j)
+        break;
+      if (vec[k] == 0)
+        continue;
+      if (i == j) {
+        res += (j == k ? vec[i] * (vec[i] - 1) * (vec[i] - 2) / 6 : 
+                         vec[i] * (vec[i] - 1) * vec[k] / 2);
+      }
+      else {
+        res += (j == k ? vec[i] * vec[j] * (vec[j] - 1) / 2 :
+                         vec[i] * vec[j] * vec[k]);
+      }
+        
+    }
+  }
+  return res % kMod;
+}
+
+int main() {
+  vector<int> A = {1,1,2,2,3,3,4,4,5,5};
+  int target = 7;
+  printf("%d", threeSumMulti(A, target));
+  getchar();
+  return 0;
+}
