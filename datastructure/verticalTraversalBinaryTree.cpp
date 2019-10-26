828f32d3b74e595eaa48e2bd56fc2b4a6c81b384
tang415a <tang415a@163.com>
2019-10-26 13:00:53 +0800

    added a case

--- datastructure/verticalTraversalBinaryTree.cpp
+++ datastructure/verticalTraversalBinaryTree.cpp
@@ -43,24 +43,61 @@ struct TreeNode{
   TreeNode(int v): val(v), left(NULL), right(NULL) {}
 };
 
-void verticalBinaryTree(TreeNode* root, vector<vector<int>>& positives, vector<vector<int>>& negatives, int val) {
+void doTraversal(TreeNode* root, vector<vector<int>>& positives, vector<vector<int>>& negatives, int val) {
   if (root == NULL)
     return;
   
-  verticalBinaryTree(root->left, positives, negatives, val - 1);
-  verticalBinaryTree(root->right, positives, negatives, val + 1);
   if (val >= 0) {
-    positives.insert()
+    if (positives.size() < val + 1)
+      positives.resize(val + 1);
+    positives[val].push_back(root->val);
   }
-    
+  else {
+    if (negatives.size() < -val)
+      negatives.resize(-val);
+    negatives[-val - 1].push_back(root->val);
+  }
+
+  doTraversal(root->left, positives, negatives, val - 1);
+  doTraversal(root->right, positives, negatives, val + 1);
 }
 
 vector<vector<int>> verticalTraversalBinaryTree(TreeNode* treeNode) {
   if (treeNode == NULL)
     return {};
-  
+  vector<vector<int>> positives, negatives, res;
+  doTraversal(treeNode, positives, negatives, 0);
+  for(int i = negatives.size() - 1; i >= 0; i--) {
+    if (negatives[i].size() > 0)
+      res.push_back(negatives[i]);
+  }
+  for (auto& iter: positives) {
+    if (iter.size() > 0)
+      res.push_back(iter);
+  }
+  return res;
 }
 
 int main() {
+  TreeNode nodes[7] = { 1, 2, 3, 4, 5, 6, 7 };
+  nodes[0].left = nodes + 1;
+  nodes[0].right = nodes + 2;
+  nodes[1].left = nodes + 3;
+  nodes[1].right = nodes + 4;
+  nodes[2].left = nodes + 5;
+  nodes[2].right = nodes + 6;
+  auto r = verticalTraversalBinaryTree(nodes);
+
+  int siz = r.size();
+  printf("[");
+  for (int i = 0; i < siz; i++) {
+    printf("[");
+    int s = r[i].size();
+    for(int j = 0; j < s; j++)
+      j == s - 1 ? printf("%d", r[i][j]) : printf("%d,", r[i][j]);
+    i == siz - 1 ? printf("]") : printf("],");
+  }
+  printf("]");
+  getchar();
   return 0;
 }
