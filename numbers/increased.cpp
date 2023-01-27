f73403569274fa7795905c61dc7458044f189ec1
tang415a <tang415a@163.com>
2023-01-27 09:35:40 +0800

    * fixed the bug of gathering sub increasing sequence

--- numbers/increased.cpp
+++ numbers/increased.cpp
@@ -29,13 +29,12 @@ int maxIncrSubSeq(int *p, int size) {
 int maxIncrSubSeq(int *p, int *g, int size) {
   int *cache = new int[size];
   cache[0] = p[0];
-  int n = 1;
+  int n = 1, e = 0;
   for (int i = 1; i < size; ++i) {
     if (p[i] > cache[n - 1]) {
       cache[n] = p[i];
       ++n;
-      for (int j = 0; j < n; ++j)
-        g[j] = cache[j];
+      e = i;
     }
     for (int j = n; j >= 0; --j) {
       if (p[i] < cache[j] && (j == 0 || p[i] > cache[j - 1])) {
@@ -44,12 +43,24 @@ int maxIncrSubSeq(int *p, int *g, int size) {
       }
     }
   }
+  int i = n - 1;
+  g[i] = cache[i];
+  while (i > 0) {
+    --e;
+    // assert(e >= 0);
+    if (p[e] < g[i] && p[e] >= cache[i - 1]) {
+      g[--i] = p[e];
+    }
+  }
   delete[] cache;
   return n;
 }
 
 int main() {
-  int arr[] = {1, 5, 2, 8, 3, 7, 4, 6, 0};
+  // int arr[] = {1, 5, 2, 8, 3, 7, 4, 6, 0};
+  // int arr[] = {1, 5, 8, 3, 7, 4, 2, 6, 0};
+  int arr[] = {1, 5, 8, 3, 7, 4, 2, 0, 1, 2, 3, 6, 0};
+  // int arr[] = {5, 4, 3, 2, 1};
   int size = sizeof(arr) / sizeof(arr[0]);
   cout << maxIncrSubSeq(arr, size) << endl;
   int *gathering = new int[size];
