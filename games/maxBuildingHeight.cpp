d21826c3a3ccea95b573b16ce265278201bd0946
tang415a <tang415a@163.com>
2023-01-31 21:11:06 +0800

    format && min pair sum case

--- games/maxBuildingHeight.cpp
+++ games/maxBuildingHeight.cpp
@@ -1,14 +1,19 @@
 /*
-You want to build n new buildings in a city. The new buildings will be built in a line and are labeled from 1 to n.
+You want to build n new buildings in a city. The new buildings will be built in
+a line and are labeled from 1 to n.
 
 However, there are city restrictions on the heights of the new buildings:
 
 The height of each building must be a non-negative integer.
 The height of the first building must be 0.
 The height difference between any two adjacent buildings cannot exceed 1.
-Additionally, there are city restrictions on the maximum height of specific buildings. These restrictions are given as a 2D integer array restrictions where restrictions[i] = [idi, maxHeighti] indicates that building idi must have a height less than or equal to maxHeighti.
+Additionally, there are city restrictions on the maximum height of specific
+buildings. These restrictions are given as a 2D integer array restrictions where
+restrictions[i] = [idi, maxHeighti] indicates that building idi must have a
+height less than or equal to maxHeighti.
 
-It is guaranteed that each building will appear at most once in restrictions, and building 1 will not be in restrictions.
+It is guaranteed that each building will appear at most once in restrictions,
+and building 1 will not be in restrictions.
 
 Return the maximum possible height of the tallest building.
 
@@ -19,24 +24,27 @@ Example 1:
 
 Input: n = 5, restrictions = [[2,1],[4,1]]
 Output: 2
-Explanation: The green area in the image indicates the maximum allowed height for each building.
-We can build the buildings with heights [0,1,2,1,2], and the tallest building has a height of 2.
+Explanation: The green area in the image indicates the maximum allowed height
+for each building. We can build the buildings with heights [0,1,2,1,2], and the
+tallest building has a height of 2.
 
 Example 2:
 
 
 Input: n = 6, restrictions = []
 Output: 5
-Explanation: The green area in the image indicates the maximum allowed height for each building.
-We can build the buildings with heights [0,1,2,3,4,5], and the tallest building has a height of 5.
+Explanation: The green area in the image indicates the maximum allowed height
+for each building. We can build the buildings with heights [0,1,2,3,4,5], and
+the tallest building has a height of 5.
 
 Example 3:
 
 
 Input: n = 10, restrictions = [[5,3],[2,5],[7,4],[10,3]]
 Output: 5
-Explanation: The green area in the image indicates the maximum allowed height for each building.
-We can build the buildings with heights [0,1,2,3,3,4,4,5,4,3], and the tallest building has a height of 5.
+Explanation: The green area in the image indicates the maximum allowed height
+for each building. We can build the buildings with heights
+[0,1,2,3,3,4,4,5,4,3], and the tallest building has a height of 5.
 
 
 Constraints:
@@ -49,13 +57,13 @@ idi is unique.
 
 */
 
-#include <vector>
-#include <iostream>
 #include <algorithm>
+#include <iostream>
+#include <vector>
+
 using namespace std;
 
-int maxBuilding(int n, vector<vector<int>> &restrictions)
-{
+int maxBuilding(int n, vector<vector<int>> &restrictions) {
   sort(begin(restrictions), end(restrictions));
   for (int s = restrictions.size(), i = s - 1; i >= 0; --i) {
     if (restrictions[i][1] >= restrictions[i][0] - 1)
@@ -63,41 +71,53 @@ int maxBuilding(int n, vector<vector<int>> &restrictions)
   }
   if (restrictions.empty())
     return n - 1;
-  for (int i = 1, s = restrictions.size(); i < s; ) {
+  for (int i = 1, s = restrictions.size(); i < s;) {
     int d = restrictions[i][0] - restrictions[i - 1][0];
     if (restrictions[i][1] - restrictions[i - 1][1] > d) {
       restrictions[i][1] = restrictions[i - 1][1] + d;
-    }
-    else if (restrictions[i - 1][1] - restrictions[i][1] > d) {
-      restrictions[i - 1][1] = restrictions[i][1] + d;
-      if (i > 1) {
-        --i;
-        continue;
-      }
+    } else if (restrictions[i - 1][1] - restrictions[i][1] > d) {
+      // restrictions[i - 1][1] = restrictions[i][1] + d;
+      // if (i > 1) {
+      //   --i;
+      //   continue;
+      // }
+      do {
+        restrictions[i - 1][1] = restrictions[i][1] + d;
+      } while (--i >= 1 && (restrictions[i - 1][1] - restrictions[i][1] >
+                            (d = restrictions[i][0] - restrictions[i - 1][0])));
     }
     ++i;
   }
 
   int r = 0, x = 1, y = 0;
-  for (auto& i : restrictions) {
+  for (auto &i : restrictions) {
     int ym = (i[0] - x + i[1] + y) / 2;
-    if (r < ym) r = ym;
+    if (r < ym)
+      r = ym;
     x = i[0];
     y = i[1];
   }
   int ym = y + n - x;
-  if (r < ym) return ym;
+  if (r < ym)
+    return ym;
   return r;
 }
 
 int main() {
-  vector<vector<int>> restrictions = {{2,1},{4,1}};
+  vector<vector<int>> restrictions = {{2, 1}, {4, 1}};
   cout << maxBuilding(5, restrictions) << endl;
   restrictions = {};
   cout << maxBuilding(6, restrictions) << endl;
-  restrictions = {{5,3},{2,5},{7,4},{10,3}};
+  restrictions = {{5, 3}, {2, 5}, {7, 4}, {10, 3}};
   cout << maxBuilding(10, restrictions) << endl;
-  restrictions = {{10,8},{12,5},{15,1}};
+  restrictions = {{10, 8}, {12, 5}, {15, 1}};
   cout << maxBuilding(20, restrictions) << endl;
+  restrictions = {{10, 9}, {15, 5}};
+  cout << maxBuilding(20, restrictions) << endl;
+  restrictions = {{10, 5}, {12, 7}, {15, 1}};
+  cout << maxBuilding(20, restrictions) << endl;
+  restrictions = {{1, 7}, {2, 7}, {3, 7}, {4, 7}, {5, 7},
+                  {7, 7}, {8, 7}, {9, 7}, {10, 7}};
+  cout << maxBuilding(10, restrictions) << endl;
   return 0;
 }
--- numbers/maxDistBetweenPair.cpp
+++ numbers/maxDistBetweenPair.cpp
@@ -1,19 +1,24 @@
 /*
-You are given two non-increasing 0-indexed integer arrays nums1​​​​​​ and nums2​​​​​​.
+You are given two non-increasing 0-indexed integer arrays
+nums1​​​​​​ and nums2​​​​​​.
 
-A pair of indices (i, j), where 0 <= i < nums1.length and 0 <= j < nums2.length, is valid if both i <= j and nums1[i] <= nums2[j]. The distance of the pair is j - i​​​​.
+A pair of indices (i, j), where 0 <= i < nums1.length and 0 <= j < nums2.length,
+is valid if both i <= j and nums1[i] <= nums2[j]. The distance of the pair is j
+- i​​​​.
 
-Return the maximum distance of any valid pair (i, j). If there are no valid pairs, return 0.
+Return the maximum distance of any valid pair (i, j). If there are no valid
+pairs, return 0.
 
-An array arr is non-increasing if arr[i-1] >= arr[i] for every 1 <= i < arr.length.
+An array arr is non-increasing if arr[i-1] >= arr[i] for every 1 <= i <
+arr.length.
 
 
 Example 1:
 
 Input: nums1 = [55,30,5,4,2], nums2 = [100,20,10,10,5]
 Output: 2
-Explanation: The valid pairs are (0,0), (2,2), (2,3), (2,4), (3,3), (3,4), and (4,4).
-The maximum distance is 2 with pair (2,4).
+Explanation: The valid pairs are (0,0), (2,2), (2,3), (2,4), (3,3), (3,4), and
+(4,4). The maximum distance is 2 with pair (2,4).
 
 Example 2:
 
@@ -35,19 +40,18 @@ Constraints:
 1 <= nums1[i], nums2[j] <= 10^5
 Both nums1 and nums2 are non-increasing.
 */
-#include <vector>
 #include <iostream>
+#include <vector>
+
 using namespace std;
 
-int maxDistance(const vector<int> &nums1, const vector<int> &nums2)
-{
+int maxDistance(const vector<int> &nums1, const vector<int> &nums2) {
   int i = 0, j = 1, m = nums1.size(), n = nums2.size(), r = 0;
   while (i < m && j < n) {
     if (nums1[i] > nums2[j]) {
       ++i;
       j = i + r + 1;
-    }
-    else {
+    } else {
       ++r;
       ++j;
     }
@@ -56,9 +60,9 @@ int maxDistance(const vector<int> &nums1, const vector<int> &nums2)
 }
 
 int main() {
-  cout << maxDistance({55,30,5,4,2}, {100,20,10,10,5}) << endl;
-  cout << maxDistance({2,2,2}, {10,10,1}) << endl;
-  cout << maxDistance({30,29,19,5}, {25,25,25,25,25}) << endl;
-  cout << maxDistance({10,7}, {13,9,8,6}) << endl;
+  cout << maxDistance({55, 30, 5, 4, 2}, {100, 20, 10, 10, 5}) << endl;
+  cout << maxDistance({2, 2, 2}, {10, 10, 1}) << endl;
+  cout << maxDistance({30, 29, 19, 5}, {25, 25, 25, 25, 25}) << endl;
+  cout << maxDistance({10, 7}, {13, 9, 8, 6}) << endl;
   return 0;
 }
