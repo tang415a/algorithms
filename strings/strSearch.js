69cc9491112b1cfc431d591e54fba49eba63736e
tang415a <tang415a@163.com>
2023-02-02 17:36:33 +0800

    tweaks

--- strings/strSearch.js
+++ strings/strSearch.js
@@ -1,14 +1,16 @@
 // Implement a sub-string search algorithm using Boyer Moore algorithm.
 // e.g. given a string "aabbccdd" and search for "bcc", then the output is: "found: from 3 to 5".
 
-let badShiftTable = {};
+// you may refer to strSearch.cpp for more
+
+const badShiftTable = {};
 function badShift(c, p) {
 	if (c in badShiftTable)
 		return badShiftTable[c];
 
 	const offset = p.length;
 	for (let i = 1; i < offset; i++) {
-		if (p[offset - 1 - i] === c) {
+		if (p[offset - 1 - i] === c) { // c also appears at length - 1 - i in pattern; we can move i
 			badShiftTable[c] = i;
 			return i;
 		}
@@ -16,6 +18,7 @@ function badShift(c, p) {
 	return offset;
 }
 
+// do i characters of suffix also appear at the head of pattern?
 function foundAtHead(i, p) {
 	const lp = p.length;
 	for (let j = 0; j < i; j++) {
@@ -25,8 +28,8 @@ function foundAtHead(i, p) {
 	return true;
 }
 
-let goodShiftTable = {};
-function goodShift(i, p) {
+const goodShiftTable = {};
+function goodShift(i, p) { // i characters have been matched (denoted by Ci)
 	if (i <= 0)
 		return 1;
 	if (i in goodShiftTable)
@@ -34,29 +37,31 @@ function goodShift(i, p) {
 
 	const lp = p.length;
 	let offset = 1;
-	for (; offset < lp - i; offset++) {
-		if (p[lp - i - 1] === p[lp - i - 1 - offset])
+	for (; offset < lp - i; offset++) {	// this iteration does not include the head since the offset never goes to lp - i (p[lp - i - offset] is the leftmost character we are trying to match)
+		if (p[lp - i - 1] === p[lp - i - 1 - offset])	// this considers the bad shift too; if the character before the Ci (lp - i - 1, i + 1 character from right) repeats, ignore it, because it fails to match (only i characters have been matched, i + 1 character from right fails)
 			continue;
 		let j = 0;
 		for (; j < i; j++) {
-			if (p[lp - i + j] !== p[lp - i + j - offset])
+			if (p[lp - i + j] !== p[lp - i + j - offset])	// the j characters of Ci match so far
 				break;
 		}
-		if (j >= i)
+		if (j >= i)	// found all the matched i characters (Ci)
 			break;
 	}
+	// found the matched i characters (Ci) elsewhere in pattern
 	if (offset < lp - i) {
 		goodShiftTable[i] = offset;
 		return offset;
 	}
 
+	// Does Ci or suffix of Ci appear at the head?
 	for (let j = i; j > 0; j--) {
-		if (foundAtHead(j, p)) {
+		if (foundAtHead(j, p)) {	// j characters from Ci (the suffix of Ci) are found at the head of pattern
 			goodShiftTable[i] = lp - j;
 			return lp - j;
 		}
 	}
-	return lp;
+	return lp;	// move the length of pattern
 }
 
 function max(a, b) {
@@ -68,7 +73,8 @@ function sub(s, p) {
 	while (offset < ls) {
 		let i = 0;
 		for (; i < lp; i++) {
-			if (p[lp - i - 1] !== s[offset - i]) {
+			if (p[lp - i - 1] !== s[offset - i]) { // dose not match
+				// use the rightmost character (either matched when i > 1 or not when i == 0, either case p[p.length - 1] would be ignored) for bad shift
 				offset += max(badShift(s[offset], p), goodShift(i, p));
 				break;
 			}
@@ -82,4 +88,5 @@ function sub(s, p) {
 		console.log('not found');
 }
 
-sub("aabbccdd", "bcc");
+// sub("aabbccdd", "bcc");
+sub("mitiozebionioiojisd", "nioio");
