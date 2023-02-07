4a7f938b5b8f6cf8008449d2d890cde573667c5c
tang415a <tang415a@163.com>
2023-02-07 17:15:53 +0800

    add comments

--- strings/longestPalindrome1.cpp
+++ strings/longestPalindrome1.cpp
@@ -127,33 +127,49 @@ int longestPalindrome2(const string &a, const string &b) {
 
 #include <algorithm>
 
+// cm1[i] means the longest common subsequence between w1[0..i] and w2
+// cm2[i] means the longest common subsequence between w2[0..i] and w1
+// cm1[s1 - 1] == cm2[s2 - 1]
 int cm1[1000], cm2[1000];
 void build_common(const string &w1, const string &w2) {
   int s1 = size(w1), s2 = size(w2);
 
+  // initiate cm1[0] and cm2[0]
   for (int j = 0, c = w1[0], prev = 0; j < s2; ++j) {
+    // set true starting from when w2 has w1[0]
     cm2[j] = prev = prev || w2[j] == c;
   }
 
+  // does w2 have w1[0]?
   cm1[0] = cm2[s2 - 1];
 
   for (int i = 1; i < s1; ++i) {
-    int c = w1[i], prev = cm2[0];
-    cm2[0] = prev || c == w2[0];
+    int c = w1[i], prev = cm2[0]; // when i is 1, cm2[0] means w2[0] == w1[0]
+    cm2[0] =
+        prev || c == w2[0]; // now cm2[0] means w2[0] == w1[0] || w2[0] == w1[1]
 
+    // compare w1[i] with w2[1..]
     for (int j = 1; j < s2; ++j) {
+      // prev++ when w2[j] = w1[i], otherwise prev = max(cm2[j], cm2[j - 1])
+      // swap cm2[j] and prev
       swap(cm2[j], prev = (w2[j] == c) ? 1 + prev : max(cm2[j], cm2[j - 1]));
     }
-
+    // cm2[s2 - 1] records the longest common subsequence between w1[0..i] and
+    // w2
     cm1[i] = cm2[s2 - 1];
   }
 }
 
 void build_pals(const string &w, int ps[]) {
   int s = size(w);
+  // the idea is to calculate lps[i]: the longest palindrome in w[i..(s-1)]
+  // update the common (ps) by adding the longest possible palindrome in
+  // w[i..(s-1)]
+  // v = 2 * common[i] + lps[i] (i < s - 1) or v = 2 * common[i] (i == s - 1)
   vector<int> pp(s);
-  pp[s - 1] = 1;
-  ps[s - 1] <<= 1;
+  pp[s - 1] = 1;   // the first round: pp[s - 1] is the longest palindrome in
+                   // w[(s-1)..(s-1)]
+  ps[s - 1] <<= 1; // 2 * ps[s - 1]
 
   if (s > 1 && ps[s - 2]) {
     ps[s - 2] = (ps[s - 2] << 1) + 1;
@@ -165,7 +181,9 @@ void build_pals(const string &w, int ps[]) {
     for (int j = i + 1, prev = 0; j < s; ++j) {
       swap(pp[j], prev = (w[i] == w[j]) ? 2 + prev : max(pp[j], pp[j - 1]));
     }
-
+    // each time pp[s-1] records the longest palindrome in w[i..(s-1)]
+    // ps[i - 1] cannot be 0 as the substring from another string must be
+    // non-empty
     if (ps[i - 1]) {
       ps[i - 1] = (ps[i - 1] << 1) + pp[s - 1];
     }
@@ -182,26 +200,37 @@ int longestPalindrome3(string word1, string word2) {
 }
 
 int main() {
+  cout << longestPalindrome3("abc", "cba") << endl;
   cout << longestPalindrome("bb", "b") << endl;
   cout << longestPalindrome2("bb", "b") << endl;
+  cout << longestPalindrome3("bb", "b") << endl;
   cout << longestPalindrome("cacbbf", "gcac") << endl;
   cout << longestPalindrome2("cacbbf", "gcac") << endl;
+  cout << longestPalindrome3("cacbbf", "gcac") << endl;
   cout << longestPalindrome("cacbbca", "bcbac") << endl;
   cout << longestPalindrome2("cacbbca", "bcbac") << endl;
+  cout << longestPalindrome3("cacbbca", "bcbac") << endl;
   cout << longestPalindrome("cacb", "cbba") << endl;
   cout << longestPalindrome2("cacb", "cbba") << endl;
+  cout << longestPalindrome3("cacb", "cbba") << endl;
   cout << longestPalindrome("ab", "ab") << endl;
   cout << longestPalindrome2("ab", "ab") << endl;
+  cout << longestPalindrome3("ab", "ab") << endl;
   cout << longestPalindrome("aa", "bb") << endl;
   cout << longestPalindrome2("aa", "bb") << endl;
+  cout << longestPalindrome3("aa", "bb") << endl;
   cout << longestPalindrome("abcd", "abcd") << endl;
   cout << longestPalindrome2("abcd", "abcd") << endl;
+  cout << longestPalindrome3("abcd", "abcd") << endl;
   cout << longestPalindrome("abcd", "dcba") << endl;
   cout << longestPalindrome2("abcd", "dcba") << endl;
+  cout << longestPalindrome3("abcd", "dcba") << endl;
   cout << longestPalindrome("aabaa", "aabb") << endl;
   cout << longestPalindrome2("aabaa", "aabb") << endl;
+  cout << longestPalindrome3("aabaa", "aabb") << endl;
   cout << longestPalindrome("abcde", "edcig") << endl;
   cout << longestPalindrome2("abcde", "edcig") << endl;
+  cout << longestPalindrome3("abcde", "edcig") << endl;
   cout << longestPalindrome("igdcef", "edcig") << endl;
   cout << longestPalindrome2("igdcef", "edcig") << endl;
   cout << longestPalindrome3("igdcef", "edcig") << endl;
